3da0492a901ff12f96d1ed8100370e9e56752a80
DarkLegend360 <darklegend360@live.com>
2020-07-05 19:06:45 +0530

    Added Navigation Menu

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.scss';
 import HomePage from './pages/homepage/homepage';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop-page/shop-page';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path="/shop" component={ShopPage}/>
